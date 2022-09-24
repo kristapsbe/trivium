9a37c112ad45afbc546ff932c2c1b772ebbdec13
Kristaps <kbebris+git@gmail.com>
2022-09-24 14:53:38 +0300

    fixed some js bugs

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,4 +1,4 @@
-currStatus = {
+var currStatus = {
     player: 0,
     board: [
         [9, 9, 9, 9, 9, 9],
@@ -13,7 +13,7 @@ currStatus = {
     maxScore: 60
 };
 
-validMoves = [
+var validMoves = [
     [9, 9, 0, 0],
     [9, 9, 0, 1],
     [9, 9, 0, 2],
@@ -23,9 +23,8 @@ validMoves = [
 ]
 
 function movePoints(board, player) {
-    for (i = board.length-1; i >= 0; i--) {
-        console.log(i)
-        for (j = 0; j < board.length; j++) {
+    for (var i = board.length-1; i >= 0; i--) {
+        for (var j = 0; j < board.length; j++) {
             if (board[i][j] == player) {
                 return (i + 1)
             }
@@ -35,9 +34,9 @@ function movePoints(board, player) {
 }
 
 function getCoords(elem) {
-    classes = $(elem).attr("class").split(/\s+/);
-    currI = -1;
-    currJ = -1;
+    var classes = $(elem).attr("class").split(/\s+/);
+    var currI = -1;
+    var currJ = -1;
     $.each(classes, function(_, v) {
         if (v.charAt(0) == 'i') {
             currI = parseInt(v.substr(1));
@@ -58,8 +57,34 @@ function getDelta(currX, nextX) {
     return 0;
 }
 
+function updatePointsScorable() {
+    for (var i = 0; i < 3; i++) {
+        var points = movePoints(currStatus.board, i);
+        $(`.player-${i}-score .player-${i}-start .cell-inner`)[0].innerHTML = points;
+        $(`.player-${i}-score span`)[0].innerHTML = currStatus.scores[i];
+        $(`.player-${i}-score .player-${i}-start`).removeClass("scorable");
+        $(`.player-${i}-target`).removeClass(`player-${i}-target`);
+        if (points > 0 && points+currStatus.scores[i] <= currStatus.maxScore) {
+            $(`.player-${i}-score .player-${i}-start`).addClass("scorable");
+        }
+    }
+    $(".active").removeClass("active");
+}
+
+function refreshValidMoves() {
+    $.ajax({
+        url: "/availableMoves",
+        type: "POST",
+        data: JSON.stringify(currStatus),
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            validMoves = data;
+        }
+    });
+}
+
 $('body').on('click', ".player-0, .player-1, .player-2", function () {
-    classes = $(this).attr("class").split(/\s+/);
+    var classes = $(this).attr("class").split(/\s+/);
     if (classes.includes(`player-${currStatus.player}`)) {
         $(".active").removeClass("active");
         $(".player-0-target").removeClass("player-0-target");
@@ -73,7 +98,7 @@ $('body').on('click', ".player-0, .player-1, .player-2", function () {
                 }
             });
         } else {
-            curr = getCoords($(this));
+            var curr = getCoords($(this));
             $.each(validMoves, function(_, v) {
                 if (v[0] == curr[0] && v[1] == curr[1]) {
                     $(`.i${v[2]}.j${v[3]}:not(.player-0):not(.player-1):not(.player-2)`).addClass(`player-${currStatus.player}-target`);
@@ -84,8 +109,8 @@ $('body').on('click', ".player-0, .player-1, .player-2", function () {
 });
 
 $('body').on('click', '.player-0-target, .player-1-target, .player-2-target', function () {
-    next = getCoords($(this));
-    curr = getCoords($(".active")[0]);
+    var next = getCoords($(this));
+    var curr = getCoords($(".active")[0]);
     if (curr[0] == -1) {
         curr = next;
     }
@@ -95,21 +120,21 @@ $('body').on('click', '.player-0-target, .player-1-target, .player-2-target', fu
         $(this).addClass(`player-${currStatus.player}`);
     } else {
         currStatus.board[curr[0]][curr[1]] = 9;
-        if (next[0] < 0 || next[0] >= currStatus.board.len || next[1] < 0 || next[1] >= currStatus.board[next[0]].len) {
+        if (next[0] < 0 || next[0] >= currStatus.board.length || next[1] < 0 || next[1] >= currStatus.board[next[0]].length) {
             currStatus.unused[currStatus.player]++;
-            $(`.player-${currStatus.player}-start:not(.player-${currStatus.player}):first`).addClass(`player-${currStatus.player}`);
+            $(`.player-${currStatus.player}-start:not(.player-${currStatus.player}):last`).addClass(`player-${currStatus.player}`);
         } else {
             currStatus.board[next[0]][next[1]] = currStatus.player;
             $(this).addClass(`player-${currStatus.player}`);
         }
         if (Math.abs(curr[0]-next[0]) > 1 || Math.abs(curr[1]-next[1]) > 1) {
-            deltaI = getDelta(curr[0], next[0]);
-            deltaJ = getDelta(curr[1], next[1]);
-            temp = [curr[0] + deltaI, curr[1] + deltaJ];
+            var deltaI = getDelta(curr[0], next[0]);
+            var deltaJ = getDelta(curr[1], next[1]);
+            var temp = [curr[0] + deltaI, curr[1] + deltaJ];
             while (next[0] != temp[0] || next[1] != temp[1]) {
                 if (currStatus.board[temp[0]][temp[1]] != 9) {
                     currStatus.unused[currStatus.board[temp[0]][temp[1]]]++;
-                    $(`.player-${currStatus.board[temp[0]][temp[1]]}-start:not(.player-${currStatus.board[temp[0]][temp[1]]}):first`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}`);
+                    $(`.player-${currStatus.board[temp[0]][temp[1]]}-start:not(.player-${currStatus.board[temp[0]][temp[1]]}):last`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}`);
                 }
                 currStatus.board[temp[0]][temp[1]] = 9;
                 $(`.i${temp[0]}.j${temp[1]}`).removeClass('player-0').removeClass('player-1').removeClass('player-2');
@@ -118,40 +143,20 @@ $('body').on('click', '.player-0-target, .player-1-target, .player-2-target', fu
             }
         }
     }
-    points = movePoints(currStatus.board, currStatus.player);
-    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start .cell-inner`)[0].innerHTML = points;
-    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).removeClass("scorable");
-    if (points+currStatus.scores[currStatus.player] <= currStatus.maxScore) {
-        $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("scorable");
-    }
     currStatus.player = (currStatus.player + 1) % 3;
     $(".curr-turn").removeClass("curr-turn");
     $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("curr-turn");
-    $(".active").removeClass("player-0").removeClass("player-1").removeClass("player-2").removeClass("active");
-    $(".player-0-target").removeClass("player-0-target");
-    $(".player-1-target").removeClass("player-1-target");
-    $(".player-2-target").removeClass("player-2-target");
-    $.ajax({
-        url: "/availableMoves",
-        type: "POST",
-        data: JSON.stringify(currStatus),
-        contentType: "application/json; charset=utf-8",
-        success: function(data) {
-            validMoves = data;
-        }
-    });
+    $(".active").removeClass("player-0").removeClass("player-1").removeClass("player-2");
+    updatePointsScorable();
+    refreshValidMoves();
 });
 
 
 $('body').on('click', '.curr-turn.scorable', function () {
-    points = movePoints(currStatus.board, currStatus.player);
-    currStatus.scores[currStatus.player] += points;
-    $(`.player-${currStatus.player}-score span`)[0].innerHTML = currStatus.scores[currStatus.player];
-    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).removeClass("scorable");
-    if (points+currStatus.scores[currStatus.player] <= currStatus.maxScore) {
-        $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("scorable");
-    }
+    currStatus.scores[currStatus.player] += movePoints(currStatus.board, currStatus.player);
+    updatePointsScorable();
     currStatus.player = (currStatus.player + 1) % 3;
     $(".curr-turn").removeClass("curr-turn");
     $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("curr-turn");
+    refreshValidMoves();
 });
