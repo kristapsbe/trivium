3fa157df830e3961e9a5741bbc649175f93a35f4
Kristaps <kbebris+git@gmail.com>
2022-09-29 19:25:51 +0300

    misunderstood what moves are valid and added checkboxed for bots

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type gameStatus struct {
-	Player   int      `json:"player"`
-	Board    [6][]int `json:"board"`
-	Unused   [3]int   `json:"unused"`
-	Scores   [3]int   `json:"scores"`
-	MaxScore int      `json:"maxScore"`
+	Player    int      `json:"player"`
+	Board     [6][]int `json:"board"`
+	Unused    [3]int   `json:"unused"`
+	Scores    [3]int   `json:"scores"`
+	MaxScore  int      `json:"maxScore"`
+	ForceMove [2]int   `json:"forceMove"`
 }
 
 func abs(a int) int {
@@ -60,21 +60,23 @@ func movePoints(board [6][]int, player int) int {
 	return 0
 }
 
-func validMoves(board [6][]int, player int, unused [3]int, leftScore int) [][4]int {
+func validMoves(board [6][]int, player int, unused [3]int, leftScore int, forceMove [2]int) [][4]int {
 	retVal := [][4]int{}
-	// we can add new pieces
-	if unused[player] > 0 {
-		for i := range board[0] {
-			// can only move to an empty cell
-			if board[0][i] == 9 {
-				retVal = append(retVal, [4]int{9, 9, 0, i})
+	if forceMove[0] == 9 {
+		// we can add new pieces
+		if unused[player] > 0 {
+			for i := range board[0] {
+				// can only move to an empty cell
+				if board[0][i] == 9 {
+					retVal = append(retVal, [4]int{9, 9, 0, i})
+				}
 			}
 		}
-	}
-	smallEnough := movePoints(board, player) <= leftScore
-	// we have at least one piece on the board - can just take points
-	if unused[player] < 3 && smallEnough {
-		retVal = append(retVal, [4]int{9, 9, 9, 9})
+		smallEnough := movePoints(board, player) <= leftScore
+		// we have at least one piece on the board - can just take points
+		if unused[player] < 3 && smallEnough {
+			retVal = append(retVal, [4]int{9, 9, 9, 9})
+		}
 	}
 	validDirs := [6][2]int{{-1, 0}, {-1, 1}, {0, -1}, {1, 0}, {1, -1}, {0, 1}}
 	for i := range board {
@@ -83,25 +85,25 @@ func validMoves(board [6][]int, player int, unused [3]int, leftScore int) [][4]i
 				for k := range validDirs {
 					deltaI := validDirs[k][0]
 					deltaJ := validDirs[k][1]
-					// we can move to empty cells
-					newI := i + deltaI
-					newJ := j + deltaJ
-					if newI >= 0 && newJ >= 0 && newI < len(board) && newJ < len(board[newI]) && board[newI][newJ] == 9 {
-						retVal = append(retVal, [4]int{i, j, newI, newJ})
+					if forceMove[0] == 9 {
+						// we can move to empty cells
+						newI := i + deltaI
+						newJ := j + deltaJ
+						if newI >= 0 && newJ >= 0 && newI < len(board) && newJ < len(board[newI]) && board[newI][newJ] == 9 {
+							retVal = append(retVal, [4]int{i, j, newI, newJ})
+						}
 					}
-					// we can kill opponents pieces
-					hopoverI := i + deltaI
-					hopoverJ := j + deltaJ
-					targetI := i + (2 * deltaI)
-					targetJ := j + (2 * deltaJ)
-					for targetI >= 0 && targetJ >= -1 && ((targetI < len(board) && targetJ <= len(board[targetI])) || (targetI == len(board) && hopoverJ == 0)) &&
-						(targetI == len(board) || targetJ == len(board[targetI]) || targetJ == -1 || board[targetI][targetJ] == 9) &&
-						board[hopoverI][hopoverJ] != 9 && board[hopoverI][hopoverJ] != player {
-						retVal = append(retVal, [4]int{i, j, targetI, targetJ})
-						hopoverI = targetI + deltaI
-						hopoverJ = targetJ + deltaJ
-						targetI += (2 * deltaI)
-						targetJ += (2 * deltaJ)
+					if forceMove[0] == 9 || (forceMove[0] == i && forceMove[1] == j) {
+						// we can kill opponents pieces
+						hopoverI := i + deltaI
+						hopoverJ := j + deltaJ
+						targetI := i + (2 * deltaI)
+						targetJ := j + (2 * deltaJ)
+						if targetI >= 0 && targetJ >= -1 && ((targetI < len(board) && targetJ <= len(board[targetI])) || (targetI == len(board) && hopoverJ == 0)) &&
+							(targetI == len(board) || targetJ == len(board[targetI]) || targetJ == -1 || board[targetI][targetJ] == 9) &&
+							board[hopoverI][hopoverJ] != 9 && board[hopoverI][hopoverJ] != player {
+							retVal = append(retVal, [4]int{i, j, targetI, targetJ})
+						}
 					}
 				}
 			}
@@ -110,75 +112,9 @@ func validMoves(board [6][]int, player int, unused [3]int, leftScore int) [][4]i
 	return retVal
 }
 
-func doMove(currStatus gameStatus) gameStatus {
-	valMoves := validMoves(currStatus.Board, currStatus.Player, currStatus.Unused, currStatus.MaxScore-currStatus.Scores[currStatus.Player])
-	moveInd := rand.Intn(len(valMoves))
-	currI := valMoves[moveInd][0]
-	currJ := valMoves[moveInd][1]
-	nextI := valMoves[moveInd][2]
-	nextJ := valMoves[moveInd][3]
-	if currI == 9 {
-		if nextI == 9 {
-			currStatus.Scores[currStatus.Player] += movePoints(currStatus.Board, currStatus.Player)
-		} else {
-			currStatus.Unused[currStatus.Player]--
-			currStatus.Board[nextI][nextJ] = currStatus.Player
-		}
-	} else {
-		currStatus.Board[currI][currJ] = 9
-		if nextI < 0 || nextI >= len(currStatus.Board) || nextJ < 0 || nextJ >= len(currStatus.Board[nextI]) {
-			currStatus.Unused[currStatus.Player]++
-		} else {
-			currStatus.Board[nextI][nextJ] = currStatus.Player
-		}
-		if abs(currI-nextI) > 1 || abs(currJ-nextJ) > 1 {
-			deltaI := getDelta(currI, nextI)
-			deltaJ := getDelta(currJ, nextJ)
-			tempI := currI + deltaI
-			tempJ := currJ + deltaJ
-			for nextI != tempI || nextJ != tempJ {
-				if currStatus.Board[tempI][tempJ] != 9 {
-					currStatus.Unused[currStatus.Board[tempI][tempJ]]++
-				}
-				currStatus.Board[tempI][tempJ] = 9
-				tempI += deltaI
-				tempJ += deltaJ
-			}
-		}
-	}
-	currStatus.Player = (currStatus.Player + 1) % 3
-
-	return currStatus
-}
-
-func fullGame() {
-	rand.Seed(time.Now().UnixNano())
-
-	currStatus := gameStatus{
-		Player: 0,
-		Board: [6][]int{
-			{9, 9, 9, 9, 9, 9},
-			{9, 9, 9, 9, 9},
-			{9, 9, 9, 9},
-			{9, 9, 9},
-			{9, 9},
-			{9},
-		},
-		Unused:   [3]int{3, 3, 3},
-		Scores:   [3]int{0, 0, 0},
-		MaxScore: 60,
-	}
-	for currStatus.Scores[0] < currStatus.MaxScore && currStatus.Scores[1] < currStatus.MaxScore && currStatus.Scores[2] < currStatus.MaxScore {
-		currStatus = doMove(currStatus)
-		printBoard(currStatus.Board)
-		printScores(currStatus.Scores, currStatus.MaxScore)
-	}
-}
-
 func main() {
 	r := gin.Default()
 
-	//fullGame()
 	r.Static("/static", "web/static")
 	r.StaticFile("/board", "web/board.html")
 	r.POST("/availableMoves", availableMoves)
@@ -192,7 +128,12 @@ func availableMoves(c *gin.Context) {
 	if err := c.BindJSON(&currStatus); err != nil {
 		return
 	}
-	c.IndentedJSON(http.StatusOK, validMoves(currStatus.Board, currStatus.Player, currStatus.Unused, currStatus.MaxScore-currStatus.Scores[currStatus.Player]))
+	valMoves := validMoves(currStatus.Board, currStatus.Player, currStatus.Unused, currStatus.MaxScore-currStatus.Scores[currStatus.Player], currStatus.ForceMove)
+	if len(valMoves) > 0 {
+		c.IndentedJSON(http.StatusOK, valMoves)
+	} else {
+		c.IndentedJSON(http.StatusOK, [][4]int{})
+	}
 }
 
 func botMove(c *gin.Context) {
@@ -200,7 +141,7 @@ func botMove(c *gin.Context) {
 	if err := c.BindJSON(&currStatus); err != nil {
 		return
 	}
-	valMoves := validMoves(currStatus.Board, currStatus.Player, currStatus.Unused, currStatus.MaxScore-currStatus.Scores[currStatus.Player])
+	valMoves := validMoves(currStatus.Board, currStatus.Player, currStatus.Unused, currStatus.MaxScore-currStatus.Scores[currStatus.Player], currStatus.ForceMove)
 	moveInd := rand.Intn(len(valMoves))
 	c.IndentedJSON(http.StatusOK, valMoves[moveInd])
 }
--- web/static/js/main.js
+++ web/static/js/main.js
@@ -10,11 +10,21 @@ var currStatus = {
     ],
     unused: [3, 3, 3],
     scores: [0, 0, 0],
-    maxScore: 60
+    maxScore: 60,
+    forceMove: [9, 9]
 };
 
 var gameOver = false;
-var botPlayers = [1, 2];
+var botPlayers = [];
+
+$('#bot1, #bot2, #bot3').change(function() {
+    botPlayers = [];
+    $.each($('#bot1, #bot2, #bot3'), function(_, elem) {
+        if ($(elem).is(':checked')) {
+            botPlayers.push(parseInt($(elem).val()));
+        }
+    });
+});
 
 var validMoves = [
     [9, 9, 0, 0],
@@ -38,8 +48,8 @@ function movePoints(board, player) {
 
 function getCoords(elem) {
     var classes = $(elem).attr("class").split(/\s+/);
-    var currI = -1;
-    var currJ = -1;
+    var currI = 9;
+    var currJ = 9;
     $.each(classes, function(_, v) {
         if (v.charAt(0) == 'i') {
             currI = parseInt(v.substr(1));
@@ -67,21 +77,36 @@ function updatePointsScorable() {
         $(`.player-${i}-score span`)[0].innerHTML = currStatus.scores[i];
         $(`.player-${i}-score .player-${i}-start`).removeClass("scorable");
         $(`.player-${i}-target`).removeClass(`player-${i}-target`);
-        if (points > 0 && points+currStatus.scores[i] <= currStatus.maxScore) {
+        if ((points > 0) && (points+currStatus.scores[i] <= currStatus.maxScore) && (currStatus.forceMove[0] == 9)) {
             $(`.player-${i}-score .player-${i}-start`).addClass("scorable");
         }
     }
     $(".active").removeClass("active");
 }
 
-function refreshValidMoves() {
+function refreshValidMoves(next) {
+    if (next[0] == 9) {
+        currStatus.player = (currStatus.player + 1) % 3;
+    }
+    $(".curr-turn").removeClass("curr-turn");
+    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("curr-turn");
+    currStatus.forceMove = next;
+    updatePointsScorable();
     $.ajax({
         url: "/availableMoves",
         type: "POST",
         data: JSON.stringify(currStatus),
         contentType: "application/json; charset=utf-8",
         success: function(data) {
-            validMoves = data;
+            console.log(data)
+            if (data.length == 0) {
+                refreshValidMoves([9, 9]);
+            } else {
+                validMoves = data;
+                if (next[0] != 9) {
+                    setActive($(`.i${currStatus.forceMove[0]}.j${currStatus.forceMove[1]}`)[0]);
+                }
+            }
         }
     });
 }
@@ -93,12 +118,8 @@ function clickScore() {
         alert(`yay ${currStatus.player} wins!`);
         gameOver = true;
     }
-    updatePointsScorable();
     $(".curr-turn").addClass(`player-${currStatus.player}-prev`);
-    currStatus.player = (currStatus.player + 1) % 3;
-    $(".curr-turn").removeClass("curr-turn");
-    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("curr-turn");
-    refreshValidMoves();
+    refreshValidMoves([9, 9]);
 }
 
 function clickMove(elem) {
@@ -123,29 +144,19 @@ function clickMove(elem) {
             $(elem).addClass(`player-${currStatus.player}`);
         }
         if (Math.abs(curr[0]-next[0]) > 1 || Math.abs(curr[1]-next[1]) > 1) {
-            var deltaI = getDelta(curr[0], next[0]);
-            var deltaJ = getDelta(curr[1], next[1]);
-            var temp = [curr[0] + deltaI, curr[1] + deltaJ];
-            while (next[0] != temp[0] || next[1] != temp[1]) {
-                if (currStatus.board[temp[0]][temp[1]] != 9) {
-                    currStatus.unused[currStatus.board[temp[0]][temp[1]]]++;
-                    $(`.i${temp[0]}.j${temp[1]}`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}-prev`);
-                    $(`.player-${currStatus.board[temp[0]][temp[1]]}-start:not(.player-${currStatus.board[temp[0]][temp[1]]}):last`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}-prev`);
-                }
-                currStatus.board[temp[0]][temp[1]] = 9;
-                $(`.i${temp[0]}.j${temp[1]}`).removeClass('player-0').removeClass('player-1').removeClass('player-2');
-                temp[0] += deltaI;
-                temp[1] += deltaJ;
+            var temp = [curr[0] + getDelta(curr[0], next[0]), curr[1] + getDelta(curr[1], next[1])];
+            if (currStatus.board[temp[0]][temp[1]] != 9) {
+                currStatus.unused[currStatus.board[temp[0]][temp[1]]]++;
+                $(`.i${temp[0]}.j${temp[1]}`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}-prev`);
+                $(`.player-${currStatus.board[temp[0]][temp[1]]}-start:not(.player-${currStatus.board[temp[0]][temp[1]]}):last`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}`).addClass(`player-${currStatus.board[temp[0]][temp[1]]}-prev`);
             }
+            currStatus.board[temp[0]][temp[1]] = 9;
+            $(`.i${temp[0]}.j${temp[1]}`).removeClass('player-0').removeClass('player-1').removeClass('player-2');
         }
     }
     $(".active").removeClass(`player-${currStatus.player}`);
     $(".active").addClass(`player-${currStatus.player}-prev`);
-    currStatus.player = (currStatus.player + 1) % 3;
-    $(".curr-turn").removeClass("curr-turn");
-    $(`.player-${currStatus.player}-score .player-${currStatus.player}-start`).addClass("curr-turn");
-    updatePointsScorable();
-    refreshValidMoves();
+    refreshValidMoves((curr[0] == 9 || Math.abs(curr[0]-next[0]) == 1 || Math.abs(curr[1]-next[1]) == 1) ? [9, 9] : next);
 }
 
 function doBotMove() {
@@ -174,29 +185,42 @@ function clearPrevs() {
     $(".cell").removeClass("player-0-prev").removeClass("player-1-prev").removeClass("player-2-prev");
 }
 
+function setActive(elem) {
+    if (currStatus.forceMove[0] == 9) {
+        clearPrevs();
+    }
+    var classes = $(elem).attr("class").split(/\s+/);
+    if (classes.includes(`player-${currStatus.player}`)) {
+        $(".active").removeClass("active");
+        $(".player-0-target").removeClass("player-0-target");
+        $(".player-1-target").removeClass("player-1-target");
+        $(".player-2-target").removeClass("player-2-target");
+        $(elem).addClass("active");
+        if (classes.includes(`player-${currStatus.player}-start`)) {
+            $.each(validMoves, function(_, v) {
+                if (v[0] == 9 && v[2] != 9) {
+                    $(`.i${v[2]}.j${v[3]}:not(.player-0):not(.player-1):not(.player-2)`).addClass(`player-${currStatus.player}-target`);
+                }
+            });
+        } else {
+            var curr = getCoords($(elem));
+            $.each(validMoves, function(_, v) {
+                if (v[0] == curr[0] && v[1] == curr[1]) {
+                    $(`.i${v[2]}.j${v[3]}:not(.player-0):not(.player-1):not(.player-2)`).addClass(`player-${currStatus.player}-target`);
+                }
+            });
+        }
+    }
+}
+
 $('body').on('click', ".player-0, .player-1, .player-2", function () {
     if (!botPlayers.includes(currStatus.player)) {
-        clearPrevs();
-        var classes = $(this).attr("class").split(/\s+/);
-        if (classes.includes(`player-${currStatus.player}`)) {
-            $(".active").removeClass("active");
-            $(".player-0-target").removeClass("player-0-target");
-            $(".player-1-target").removeClass("player-1-target");
-            $(".player-2-target").removeClass("player-2-target");
-            $(this).addClass("active");
-            if (classes.includes(`player-${currStatus.player}-start`)) {
-                $.each(validMoves, function(_, v) {
-                    if (v[0] == 9 && v[2] != 9) {
-                        $(`.i${v[2]}.j${v[3]}:not(.player-0):not(.player-1):not(.player-2)`).addClass(`player-${currStatus.player}-target`);
-                    }
-                });
-            } else {
-                var curr = getCoords($(this));
-                $.each(validMoves, function(_, v) {
-                    if (v[0] == curr[0] && v[1] == curr[1]) {
-                        $(`.i${v[2]}.j${v[3]}:not(.player-0):not(.player-1):not(.player-2)`).addClass(`player-${currStatus.player}-target`);
-                    }
-                });
+        if (currStatus.forceMove[0] == 9) {
+            setActive($(this));
+        } else {
+            var classes = $(this).attr("class").split(/\s+/);
+            if (classes.includes("active")) {
+                refreshValidMoves([9, 9]);
             }
         }
     }
